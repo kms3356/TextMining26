1de89218a6557476c3cebc8546f387adae7eafee
kms3356 <john563322@gmail.com>
2026-05-06 01:58:36 +0900

    5.6 naver crawl

---
+++ NaverNewsCrawler/NaverNewsCrawler.py
@@ -0,0 +1,53 @@
+import urllib.request
+import json
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+naver_headers = {
+    "X-Naver-Client-Id": client_id,
+    "X-Naver-Client-Secret": client_secret
+}
+
+def crawlNaverNews(url, start=1, display=10):
+    url += f'&start={start}&display={display}'
+    request = urllib.request.Request(url, headers=naver_headers)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode() 
+
+    if(rescode==200): # HTTP 응답 코드가 200이면 성공적으로 데이터를 가져온 것
+        news_data = response.read().decode('utf-8')
+        return json.loads(news_data) # JSON 문자열을 Python 객체로 변환하여 반환
+    else:
+        print("Error Code:" + rescode)
+        return None
+    
+def mergeResultToList(result_list, merged_list):
+    if result_list is not None:
+        merged_list += result_list['items']
+        return True
+    else: return False
+
+def saveJsonToCSV(merged_list, csv_filename):
+    df = pd.DataFrame(merged_list)
+    df.to_csv(f'result/{csv_filename}', index=False, encoding='utf-8-sig')
+
+def crawlMain(keyword):
+    encText = urllib.parse.quote(keyword)
+    url = "https://openapi.naver.com/v1/search/news?query=" + encText
+    news_corpus = []
+    start = 1
+    display = 10
+
+    while start <= 100:
+        news_data = crawlNaverNews(url, start, display)
+        mer = mergeResultToList(news_data, news_corpus)
+        if not mer: break
+        start += display
+    saveJsonToCSV(news_corpus, 'news_corpus.csv')
+    return news_corpus
+
--- Selenium.py
+++ Selenium.py
@@ -6,6 +6,9 @@ driver = webdriver.Chrome()
 
 # 2. 페이지 이동
 driver.get("https://www.naver.com")
+ 
+driver.execute_script('script_string')  # 자바스크립트 실행 예시 (필요한 경우)
+html_doc = driver.page_source  # 현재 페이지의 HTML 소스 가져오기
 
 # 3. 동작 수행 (예: 검색창에 단어 입력 후 엔터)
 search_box = driver.find_element(By.ID, "query")
